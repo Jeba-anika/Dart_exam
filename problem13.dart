e59508894ea5f5148b2c407d27bd60195b072009
Jeba-anika <anikanur98@gmail.com>
2022-06-18 23:55:36 +0600

    second commit

--- problem13.dart
+++ problem13.dart
@@ -1,9 +1,5 @@
 void main() {
   List number = [29, 76, 12, 32];
-  List number2 = [];
-  for(int i = 0; i<= number.length; i++){
-    for(int j = 0; j<=number.length; j++){
-      if()
-    }
-  }
+  number.sort();
+  print(number);
 }
