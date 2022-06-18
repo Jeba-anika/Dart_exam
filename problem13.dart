a9b6dd6b53774cca9e9ed5faa4efc55f1c3dd570
Jeba-anika <anikanur98@gmail.com>
2022-06-18 23:50:04 +0600

    first commit

---
+++ problem13.dart
@@ -0,0 +1,9 @@
+void main() {
+  List number = [29, 76, 12, 32];
+  List number2 = [];
+  for(int i = 0; i<= number.length; i++){
+    for(int j = 0; j<=number.length; j++){
+      if()
+    }
+  }
+}
